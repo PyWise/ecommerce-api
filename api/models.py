b81b5a4926c1bff9414af7d9edfea67234873579
Victor <victorfariasbezerra12@gmail.com>
2025-07-06 16:34:00 -0300

    building views and urls

--- api/models.py
+++ api/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
     image = models.ImageField(upload_to="product_img", blank=True, null=True)
     category = models.ForeignKey(
         Category,
@@ -33,6 +33,7 @@ class Product(models.Model):
         blank=True,
         null=True,
     )
+    featured = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
--- api/serializers.py
+++ api/serializers.py
@@ -2,14 +2,26 @@ from rest_framework import serializers
 from .models import Product, Category
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "name", "slug", "description", "image", "price"]
+        fields = ["id", "name", "slug", "image", "price"]
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    products = ProductSerializer(many=True, read_only=True)
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "description", "slug", "image", "price"]
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "image", "slug"]
+
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
 
     class Meta:
         model = Category
